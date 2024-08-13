fa69968e2235c35cfc4f1bda7f31b16516ed5718
ItsDuy <duy.nguyen280805@hcmut.edu.vn>
2024-08-13 11:30:06 +0700

    inatial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SumofArray;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the desired size of array:");
+        int size;
+        int.TryParse(Console.ReadLine(), out size);
+        int[] array = new int[size];
+        int total=0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine($"Enter the desired value of array [{i}]:");
+            int.TryParse(Console.ReadLine(), out array[i]);
+            total+=array[i];
+        }
+        Console.WriteLine($"Total value of given array: {total}");
+    }
+}
